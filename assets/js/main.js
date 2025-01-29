3acaf3d5d45857749720d74735168243ea8bf619
Lucas <lucasfreitasdecarvalho15@gmail.com>
2025-01-29 17:19:37 -0300

    Adição do id do Pokemon na url

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <a href="/segunda-pagina.html"><li class="pokemon ${pokemon.type}">
+        <a href="/segunda-pagina.html?id=${pokemon.id}"><li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
